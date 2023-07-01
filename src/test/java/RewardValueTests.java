37481a3d50821be81cd450c7c2ce22371f50a555
Terence Johnson <tejohn@gmail.com>
2023-07-01 15:22:18 -0400

    Adds Rewards Converter tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +20,16 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 375;
+        var rewardValue = new RewardValue(cashValue);
+        int expextedMilesValue = (int)(cashValue / MILES_TO_CASH_CONVERSION_RATE);
+        assertEquals(expextedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
-    }
-}
+        int milesValue = 3000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
+}}
